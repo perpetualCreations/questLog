cc4c0a886338279ddd82a672730f88265565bada
tchen <tchen0584@gmail.com>
2021-07-13 15:57:04 -0400

    Move to Flask, database initialization and SSL.

--- main.py
+++ main.py
@@ -5,33 +5,35 @@ Contains responses to URIs.
 """
 
 import configparser
-import bottle
-import redis
 from json import loads as json_loads
+from ast import literal_eval
+import flask
+import redis
 
 
 config = configparser.ConfigParser()
 config.read("main.cfg")
+application = flask.Flask(__name__)
 PASSWORD = config["database"]["password"]
 if config["database"]["password"].lower() == "none":
     PASSWORD = None
+SSL_PARAMETERS = {}
+if literal_eval(config["database"]["ssl"]) is True:
+    SSL_PARAMETERS = {"ssl_keyfile": config["database"]["ssl_key_path"],
+                      "ssl_certfile": config["database"]["ssl_cert_path"],
+                      "ssl_cert_reqs": "required",
+                      "ssl_ca_certs":
+                          config["database"]["ssl_cert_authority_cert_path"]}
 database = redis.Redis(config["database"]["hostname"],
                        int(config["database"]["port"]), 0,
-                       PASSWORD)
+                       PASSWORD, **SSL_PARAMETERS)
 if database.get("initialized") is not True:
-    pass
+    with open("json/root.json") as template_handler:
+        template = json_loads(template_handler.read())
+    for key in template:
+        database.set(key, template[key])
 
 
-@bottle.hook("after_request")
-def cors_enable():
-    """Define headers to enable Cross-Origin resource sharing."""
-    bottle.response.headers["Access-Control-Allow-Origin"] = "*"
-    bottle.response.headers["Access-Control-Allow-Methods"] = \
-        "PUT, GET, POST, DELETE, OPTIONS"
-    bottle.response.headers["Access-Control-Allow-Headers"] = \
-        "Authorization, Origin, Accept, Content-Type, X-Requested With"
-
-
-@bottle.route("/api/", method="GET")
-def api_get_handle():
-    """Respond to GET requests for API."""
+@application.route("/api/user/<username>", methods=["PUT"])
+def api_get_handle(username):
+    """Respond to PUT requests for user management API."""
